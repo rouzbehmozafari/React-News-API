90f6fd31d92ea22922b66017422cd7e47e1c7361
“rouzbehmozafari” <rouzbeh.mozafari@yahoo.com>
2021-12-15 15:52:55 +0100

    first limit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import NewsItem from './NewsItem';
 import './App.css';
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      url : 'http://newsapi.org/v2/everything?' +
+      'q=Apple&' +
+      'from=2021-12-15&' +
+      'sortBy=popularity&' +
+      'apiKey=a939f33eb3d94a36acd4ccb59fdc33a9' ,
+      isLoaded : false,
+      articles  : []
+     }
+  }
+componentDidMount(){
+  var req = new Request(this.state.url);
+  var req = this.state.url
+  fetch(req)
+  .then(response => response.json())
+  .then(json => this.setState({
+    articles: json ,
+    isLoaded : true
+  }))
+} 
+
+show = () => {
+  {console.log(this.state.articles[0].title)}
+  {console.log(this.state.isLoaded)}
 }
-
+  render() { 
+      return ( 
+      <div className="App">
+          <div>
+            <h1>News</h1>
+              if (this.state.isLoaded) {
+                // console.log(this.state.articles)
+                this.state.articles.map((a) =>
+                    <NewsItem 
+                      key = {uuidv4()}
+                      urlToImage = {a.urlToImage}
+                      title = {a.title}
+                      description = {a.description}
+                      publishedAt = {a.publishedAt}
+                      url = {a.url}
+                    />
+                  )
+              }
+          </div>
+  </div> );
+    }
+  }
 export default App;
---
+++ src/NewsItem.js
@@ -0,0 +1,13 @@
+const NewsItem = (props) => {
+    return ( 
+        <div className="NewsItem">
+            <img urlToImage={props.urlToImage} alt="img" />
+            <h4>{props.title}</h4>
+            <p>{props.description}</p>
+            <p>{props.publishedAt}</p>
+            <a href={props.url}>Read More</a>
+        </div>
+     );
+}
+ 
+export default NewsItem;
