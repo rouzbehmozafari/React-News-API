787e29c3f04765caf9a02fd251ec787a61cf47d2
“rouzbehmozafari” <rouzbeh.mozafari@yahoo.com>
2021-12-15 23:36:48 +0100

    test-temp

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import './App.css';
 import { v4 as uuidv4 } from 'uuid';
 
 class App extends Component {
+
+
   constructor(props) {
     super(props);
     this.state = { 
-      url : 'http://newsapi.org/v2/everything?' +
-      'q=Apple&' +
-      'from=2021-12-15&' +
-      'sortBy=popularity&' +
-      'apiKey=a939f33eb3d94a36acd4ccb59fdc33a9' ,
+      // url : 'http://newsapi.org/v2/everything?' +
+      // 'q=Apple&' +
+      // 'from=2021-12-15&' +
+      // 'sortBy=popularity&' +
+      // 'apiKey=a939f33eb3d94a36acd4ccb59fdc33a9' ,
+      url: 'https://jsonplaceholder.typicode.com/posts',
       isLoaded : false,
       articles  : []
      }
   }
 componentDidMount(){
-  var req = new Request(this.state.url);
+  // var req = new Request(this.state.url);
   var req = this.state.url
   fetch(req)
   .then(response => response.json())
@@ -27,25 +30,22 @@ componentDidMount(){
   }))
 } 
 
-show = () => {
-  {console.log(this.state.articles[0].title)}
-  {console.log(this.state.isLoaded)}
-}
   render() { 
       return ( 
       <div className="App">
           <div>
             <h1>News</h1>
-              if (this.state.isLoaded) {
-                // console.log(this.state.articles)
+              {/* if (this.state.isLoaded)  */}
+              {
+
                 this.state.articles.map((a) =>
                     <NewsItem 
                       key = {uuidv4()}
-                      urlToImage = {a.urlToImage}
+                      // urlToImage = {a.urlToImage}
                       title = {a.title}
-                      description = {a.description}
-                      publishedAt = {a.publishedAt}
-                      url = {a.url}
+                      // description = {a.body}
+                      // publishedAt = {a.publishedAt}
+                      // url = {a.url}
                     />
                   )
               }
