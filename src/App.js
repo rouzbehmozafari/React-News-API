60fbb85216c7312d9744511d7d668cf880227344
“rouzbehmozafari” <rouzbeh.mozafari@yahoo.com>
2021-12-16 01:07:05 +0100

    land1

--- src/App.js
+++ src/App.js
@@ -19,21 +19,42 @@ class App extends Component {
       articles  : []
      }
   }
+
 componentDidMount(){
   var req = new Request(this.state.url);
   // var req = this.state.url
   fetch(req)
   .then(response => response.json())
   .then(json => this.setState({
-    articles: json.articles ,
-    isLoaded : true
-  }))
+    articles: json.articles 
+  })) 
 } 
+change = ()=> {
+  let land = document.getElementById('land').value
+  var selectedLand = 'https://newsapi.org/v2/top-headlines?' +
+  'country='+ land +'&' +
+  'apiKey=a939f33eb3d94a36acd4ccb59fdc33a9';
+  // this.setState({isLoaded: true})
+  // this.setState({url: selectedLand})
+  fetch(selectedLand)
+  .then(response => response.json())
+  .then(json => this.setState({
+    articles: json.articles 
+  }))
+
+}
 
   render() { 
       return ( 
       <div className="App">
           <div>
+            <select onChange={this.change} name="land" id="land">
+              <option value="us">us</option>
+              <option value="de">de</option>
+              <option value="gb">gb</option>
+              <option value="gb">fr</option>
+              <option value="gb">pl</option>
+            </select>
             <h1>News</h1>
             <div className="newsC">
                             {
